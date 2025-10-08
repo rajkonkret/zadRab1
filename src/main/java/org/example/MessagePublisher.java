e20471af8f9cbd8c321b0eabb0ce05160cecb3f9
rajkonkret <rajkonkret660@gmail.com>
2025-10-08 12:59:45 +0200

    [1 6] add MessagePublisher

--- src/main/java/org/example/MessagePublisher.java
+++ src/main/java/org/example/MessagePublisher.java
@@ -1,14 +1,30 @@
 package org.example;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class MessagePublisher {
 
-        // psvm tabulator
-    public static void main(String[] args) {
+    // psvm tabulator
+    public static void main(String[] args) throws IOException, TimeoutException {
         System.out.printf("Hello and welcome!");
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        Connection connection = factory.newConnection(CommonConfig.AMQP_URL);
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(CommonConfig.DEFAULT_QUEUE, true, false, false, null);
+
+        for (int i = 0; i < 4; i++) {
+            String message = "Getting - MSQ" + i;
+            channel.basicPublish("", CommonConfig.DEFAULT_QUEUE, null, message.getBytes());
+        }
+
+        channel.close();
+        connection.close();
     }
 }
