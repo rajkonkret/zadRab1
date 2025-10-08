ad5d5fe7a8ae56eae7e81effc1e2c5dcaa9e95e7
rajkonkret <rajkonkret660@gmail.com>
2025-10-08 15:15:05 +0200

    [1 14] add  Fanout Exchange

--- src/main/java/org/example/fanoutexchnge/FanotExchange.java
+++ src/main/java/org/example/fanoutexchnge/FanotExchange.java
@@ -1,15 +1,64 @@
 package org.example.fanoutexchnge;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import org.example.ConnectionManager;
 
+import javax.swing.*;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class FanotExchange {
 
-    public static void declareExchange() {
+    public static void declareExchange() throws IOException, TimeoutException {
         Channel channel = ConnectionManager.getConnection().createChannel();
-    }
-    public static void main(String[] args) {
 
+        channel.exchangeDeclare("my-fanout-exchange", BuiltinExchangeType.FANOUT, true);
+        channel.close();
+    }
+
+    public static void declareBindings() throws IOException, TimeoutException {
+        Channel channel = ConnectionManager.getConnection().createChannel();
+
+        channel.queueBind("MobileQ", "my-fanout-exchange", "");
+        channel.queueBind("ACQ", "my-fanout-exchange", "");
+        channel.queueBind("LightQ", "my-fanout-exchange", "");
+
+        channel.close();
+    }
+
+    public static void subscribeMessages() throws IOException {
+        Channel channel = ConnectionManager.getConnection().createChannel();
+
+        channel.basicConsume("LightQ", true, (
+                        (consumerTag, message) -> {
+                            System.out.println(consumerTag);
+                            System.out.println("LightQ: " + new String(message.getBody()));
+                        }),
+                consumerTag -> {
+                    System.out.println(consumerTag);
+                });
+
+        channel.basicConsume("ACQ", true, (
+                        (consumerTag, message) -> {
+                            System.out.println(consumerTag);
+                            System.out.println("ACQ: " + new String(message.getBody()));
+                        }),
+                consumerTag -> {
+                    System.out.println(consumerTag);
+                });
+
+        channel.basicConsume("MobileQ", true, (
+                        (consumerTag, message) -> {
+                            System.out.println(consumerTag);
+                            System.out.println("MobileQ: " + new String(message.getBody()));
+                        }),
+                consumerTag -> {
+                    System.out.println(consumerTag);
+                });
+    }
+
+    public static void main(String[] args) {
 
 
     }
