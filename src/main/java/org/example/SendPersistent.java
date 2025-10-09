2d51b1a1013526b10ec3635a188f86ff3aafaf82
rajkonkret <rajkonkret660@gmail.com>
2025-10-09 12:42:03 +0200

    [2 20] add SendPersistent

---
+++ src/main/java/org/example/SendPersistent.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class SendPersistent {
+
+    private final static String QUEUE_NAME = "persist_queue";
+
+    public static void main(String[] args) throws IOException {
+
+        Channel channel = ConnectionManager.getConnection().createChannel();
+
+        boolean durable = true;
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+
+        String message = "Persist Messaage";
+        channel.basicPublish("",
+                QUEUE_NAME,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes(StandardCharsets.UTF_8));
+
+        System.out.println("Wysłano:" + message);
+
+    }
+}
