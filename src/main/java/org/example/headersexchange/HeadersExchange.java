27f5556c453a3eb489a6237edcc4e9eaf2ef19ea
rajkonkret <rajkonkret660@gmail.com>
2025-10-09 09:46:53 +0200

    [2 4] update HeadersExchange

--- src/main/java/org/example/headersexchange/HeadersExchange.java
+++ src/main/java/org/example/headersexchange/HeadersExchange.java
@@ -5,6 +5,9 @@ import com.rabbitmq.client.Channel;
 import org.example.ConnectionManager;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
 public class HeadersExchange {
@@ -26,5 +29,23 @@ public class HeadersExchange {
         channel.queueDeclare("VercomQ", true, false, false, null);
 
         channel.close();
+    }
+
+    public static void declareBindings() throws IOException {
+        Channel channel = ConnectionManager.getConnection().createChannel();
+
+        // tworzenie linków (bindingów)
+        // {"arg" : "wartość"}
+        Map<String, Object> healthArgs = new HashMap<>();
+        healthArgs.put("x-match", "any"); // any - dowalny
+        healthArgs.put("h1", "Header1");
+        healthArgs.put("h2", "Header2");
+        channel.queueBind("HealthQ", "my-header-exchange", "", healthArgs);
+
+        Map<String, Object> sportsArgs = new HashMap<>();
+        healthArgs.put("x-match", "all"); // all - wszystkie
+        healthArgs.put("h1", "Header1");
+        healthArgs.put("h2", "Header2");
+        channel.queueBind("SportsQ", "my-header-exchange", "", sportsArgs);
     }
 }
